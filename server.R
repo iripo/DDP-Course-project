a910aaa6546540a5e6d7a37f7cadc17b6ef82d1a
iripo <irinysin@gmail.com>
2016-01-31 10:21:34 +0300

    Add comments in script

--- server.R
+++ server.R
@@ -1,8 +1,23 @@
+# Shiny Application Project for the Developing Data Products course
+#
+# Iriny Nisina
+# 31 January 2016
+#
+# The dataset is public available for research. The details are described in [Cortez et al., 2009]. 
+# 
+# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis. 
+# Modeling wine preferences by data mining from physicochemical properties.
+# In Decision Support Systems>, Elsevier, 47(4):547-553. ISSN: 0167-9236.
+# 
+# Available at: [@Elsevier] http://dx.doi.org/10.1016/j.dss.2009.05.016
+# [Pre-press (pdf)] http://www3.dsi.uminho.pt/pcortez/winequality09.pdf
+# [bib] http://www3.dsi.uminho.pt/pcortez/dss09.bib
+
 library(shiny)
 library(lattice)
 library(caret)
 
-
+#Loading data and some simple manipulations with data variables format.
 Data <- read.csv2("./data/winequality-red.csv")
 Data$fixed.acidity <- as.numeric(as.character(Data$fixed.acidity))
 Data$volatile.acidity <- as.numeric(as.character(Data$volatile.acidity))
@@ -19,37 +34,36 @@ Data$quality <- as.factor(Data$quality)
 
 shinyServer(function(input, output) {
   
+#Compute formula for densityplots tabpanel.  
   formula1 <- reactive({
     paste("~",input$variable, " | quality")
   })
     
-  output$caption <- renderText({
-    formula1()
-  })
-
+#Compute two formulas for pairs plots and Summary tabpanels.
   formula3 <- reactive({
     paste(input$checkGroup,sep="+",collapse="+")
   })
-  
-  output$caption <- renderText({
-    formula3()
-  })
+
 
 formula2 <- reactive({
   if (length(input$checkGroup) > 0) {paste("as.numeric(quality)","~",formula3())}
   else {paste("as.numeric(quality)","~.")}
   })
-  output$caption <- renderText({
-    formula2()
-  })
-  
+
+#Generate Pairs plotwith variable chosen by answering question 
+#What variables should be the predictors in linear model to predict 
+#Quality of wine on the left sidepanel. 
   output$Pairs <- renderPlot({
     pairs(as.formula(formula2()), 
             data = Data,
             col="green",
             main="Scatterplot Matrices")
   })
-  
+
+#Generate Density plots chosen by answering question 
+#Select what characteristic of wine would you like to plot by quality score? 
+#characteristics by Quality score. 
+
 output$Plot <- renderPlot({
   densityplot(as.formula(formula1()), 
               data = Data,
@@ -58,6 +72,11 @@ output$Plot <- renderPlot({
               sub="3-3 Scored, 4-4 Scored, 5-5 Scored, 6-6 Scored, 7-7 Scored, 8-8 Scored",    
               plot.points = FALSE, ref = TRUE)
 })
+
+#Generate Summary of the Linear Model with the predictors selected by answering question 
+#What variables should be the predictors in linear model to predict 
+#Quality of wine on the left sidepanel. 
+
   output$summary <- renderPrint({
     set.seed(12344)
     modFit <- train(as.formula(formula2()), data = Data,
--- ui.R
+++ ui.R
@@ -1,23 +1,36 @@
+# Shiny Application Project for the Developing Data Products course
+#
+# Iriny Nisina
+# 31 January 2016
+#
+# The dataset is public available for research. The details are described in [Cortez et al., 2009]. 
+# 
+# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis. 
+# Modeling wine preferences by data mining from physicochemical properties.
+# In Decision Support Systems>, Elsevier, 47(4):547-553. ISSN: 0167-9236.
+# 
+# Available at: [@Elsevier] http://dx.doi.org/10.1016/j.dss.2009.05.016
+# [Pre-press (pdf)] http://www3.dsi.uminho.pt/pcortez/winequality09.pdf
+# [bib] http://www3.dsi.uminho.pt/pcortez/dss09.bib
+
+
+
 library(shiny)
 
-#P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis. Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009. 
 shinyUI(fluidPage(
+  
+  #Application title  
   titlePanel("My Shiny App - Red Wine characteristics by quality scores"),
+
   sidebarLayout(
+
+    #Sidebarpanel with controls to select: 
+    # 1) the characteristics of wine that could be chosen to plot by quality score in density plots.  
+    # 2) the characteristrics (variables) that could be chosen 
+    #    - to explore the pairwise relations between variables
+    #    - as predictors in linear model to predict Quality of wine
+
     sidebarPanel(
-    checkboxGroupInput("checkGroup", 
-                   label = h3("What variables should be the predictors in linear model to predict Quality of wine"), 
-                   choices = list("fixed acidity" = "fixed.acidity", 
-                   "volatile acidity" = "volatile.acidity", 
-                   "citric acid" = "citric.acid",
-                   "residual sugar"="residual.sugar",
-                   "chlorides"="chlorides",
-                   "free sulfur dioxide" = "free.sulfur.dioxide",
-                   "total sulfur dioxide"="total.sulfur.dioxide",
-                   "density"="density",
-                   "pH"="pH",
-                   "sulphates"="sulphates",
-                   "alcohol"="alcohol")), 
     selectInput("variable", "Select what characteristic of wine would you like to plot by quality score?",
                   list("fixed acidity" = "fixed.acidity", 
                        "volatile acidity" = "volatile.acidity", 
@@ -31,8 +44,24 @@ shinyUI(fluidPage(
                        "sulphates"="sulphates",
                        "alcohol"="alcohol"                       
                        
-      ))
-    ),  
+      )),
+    checkboxGroupInput("checkGroup", 
+                       label = h3("What variables should be the predictors in linear model to predict Quality of wine"), 
+                       choices = list("fixed acidity" = "fixed.acidity", 
+                                      "volatile acidity" = "volatile.acidity", 
+                                      "citric acid" = "citric.acid",
+                                      "residual sugar"="residual.sugar",
+                                      "chlorides"="chlorides",
+                                      "free sulfur dioxide" = "free.sulfur.dioxide",
+                                      "total sulfur dioxide"="total.sulfur.dioxide",
+                                      "density"="density",
+                                      "pH"="pH",
+                                      "sulphates"="sulphates",
+                                      "alcohol"="alcohol"))
+  ),
+  
+  #Show two plots and characteristics of the built model
+  
 mainPanel(
   tabsetPanel(type = "tabs", 
               tabPanel("Plot", plotOutput("Plot")), 
